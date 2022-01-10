ebf1601af4f056770617ecaa8133dd5ab70f18a7
Lucas Goeten <lucasgoetengithub@gmail.com>
2022-01-10 13:10:59 -0300

    Asserthat

--- JUnit/Teste unitario/TestesUnitarios/src/test/java/br/ce/lgoeten/servicos/LocacaoServiceTest.java
+++ JUnit/Teste unitario/TestesUnitarios/src/test/java/br/ce/lgoeten/servicos/LocacaoServiceTest.java
@@ -1,8 +1,13 @@
 package br.ce.lgoeten.servicos;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+
 import java.util.Date;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import br.ce.lgoeten.entidades.Filme;
@@ -23,8 +28,9 @@ public class LocacaoServiceTest {
 		Locacao locacao = locacaoService.alugarFilme(usuario, filme);
 
 		// verificacoes
-		Assert.assertEquals("Erro comparação valor da locacao",5.0 , locacao.getValor(), 0.1);
-		Assert.assertTrue(DataUtils.isMesmaData(new Date(), locacao.getDataLocacao()));
-		Assert.assertTrue(DataUtils.isMesmaData(DataUtils.obterDataComDiferencaDias(1), locacao.getDataRetorno()));
+		assertEquals("Erro comparação valor da locacao",5.0 , locacao.getValor(), 0.1);
+		assertThat(locacao.getValor(), is(equalTo(5.0)));
+		assertTrue(DataUtils.isMesmaData(new Date(), locacao.getDataLocacao()));
+		assertTrue(DataUtils.isMesmaData(DataUtils.obterDataComDiferencaDias(1), locacao.getDataRetorno()));
 	}
 }
